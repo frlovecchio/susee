ee88cafa4ff87e0bb6186b6a84837693a52728c2
susee <frlovecchio@outlook.it>
2023-04-10 17:56:26 +0200

    ver. 2.0 setup: number of attempts to connect to the device

--- setup.py
+++ setup.py
@@ -5,31 +5,34 @@ with open('README.md', "r") as fh:
 
 setuptools.setup(
     name="susee",
-    version='1.7.2',
-    author="eng. F.S. Lovecchio, ph.D - Bari, Italy",
+    version='2.0',
+    author="eng. Francesco Saverio Lovecchio, ph.D - Bari, Italy",
     author_email="frlovecchio@outlook.it",
-    description="suSEE - Energy Monitoring Platform",
+    description="suSEE - Energy Monitoring System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/frlovecchio/susee',
 
-    packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(),
+    packages=['susee'],
 
     install_requires=[
-                'numpy',
-                'pandas',
-                'mysql-connector',
-                'pymodbus',
-                'pytz',
-                'scipy',
-                'statsmodels',
-                'python-decouple',
-                'setuptools',
-                'wheel',
+        'numpy',
+        'pandas',
+        'mysql-connector',
+        'pymodbus=>3.2.2',  #requires python >3.8
+        'pytz',
+        'scipy',
+        'statsmodels',
+        'python-decouple',
+        'setuptools',
+        'wheel',
+        'bokeh==1.3.4',  # 2.4.3',
     ],
    
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Topic :: System :: Monitoring',
@@ -38,5 +41,5 @@ setuptools.setup(
     ],
 
     package_dir={"": "src"},
-    python_requires=">=3.5.0",
+    python_requires=">=3.8.0",
 )
--- tests/etl_data.py
+++ tests/etl_data.py
@@ -29,16 +29,32 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
+
 ver = '1.6 - 2022-02-07'
-ver = '1.7 - 2022-02-14'
+ver = '1.7 - 2022-06-02'
+ver = '1.8 - 2022-06-23'
+ver = '2.0 - 2022-07-10'
+ver = '2.1 - 2022-07-24'
+ver = '2.2 - 2022-07-31'
+ver = '2.31 - 2022-08-23'
+ver = '2.32 - 2022-09-10'
+ver = '2.33 - 2022-09-30'
+ver = '2.34 - 2022-10-03'
+ver = '3.0  - 2022-11-12' #added class events
+ver = '3.1  - 2022-11-16' #added class events
+
+
 
 sample_params = {
-    'sampleTime': 10,  # [s]
-    'sampleDelay': 0,  # [s]
-    'sampleShift': 0,  # [s]
-    'sampleType': 0,
+    'sampleTime': 10,   # [s]
+    'sampleShift': 0,   # [s]
+    'sampleType': 0,    #0: endless
+    'waitTime' : 0.0,  #s   wait time after close connection with devices (default=0)
     'utc_time': True,
+    'open_close_conn': False, #True: open and close connection at any cycle
+    'num_try' : 3 #Numbr of attempts to read registers
 }
+
 
 # Opzioni
 options_ = {
@@ -52,10 +68,78 @@ options_ = {
     'lastData': True,
 }
 
+
+# Opzioni
+options_ = {
+    'LogFile': True,
+    'Messages': True,
+    'datafileBk': False,    # backup file - config_data_bk{}
+    'tempfileBk': True,     # temp-backup file waiting at database connection - config_mysql_bk{}
+    'tempArrayBk': False,
+    'threads': False,       #True: reads devices; False: read servers
+    'dataDdBk': True,
+    'lastData': True,
+    'custom': True,
+    'fastSample': True,     #fast sample for CABIN's devices and SCADA report
+    'UpgradeWeb': False,     #Ugrade sysTable of web interface
+}
+
+from decouple import config
+
+user_db = config('user_db', default='username')
+pswd_db = config('pswd_db', default='password')
+host_db = config('host_db', default='127.0.0.1')
+port_db = config('port_db', default='3306')
+job_id = config('job_id', default='000')
+
+res_data = {
+    'user': user_db,
+    'passwd': pswd_db,
+    'host': host_db,  # '10.8.0.1',
+    'port': port_db,
+}
+
+config_mysql = {
+    **res_data,
+
+    'database': 'seedb' + job_id,
+    'tableRaw': 'see' + job_id + 'Raw',
+    'tableDeps': 'see' + job_id + 'Deps',
+    'tableEvents': 'see' + job_id + 'Events',
+    'tableEventsSys': 'see' + job_id + 'EventsSys',
+    'tableJoin': 'see' + job_id + 'Join',
+    'tableLast': 'see' + job_id + 'Last',
+    'tableLastFast': 'see' + job_id + 'LastFast',
+    'tableMac': 'see' + job_id + 'Mac',
+    'tableParams': 'see' + job_id + 'Params',
+    'tableProd': 'see' + job_id + 'Prod',
+    'tableSysRaw': 'see' + job_id + 'SysSetRaw',
+    'tableSys': 'see' + job_id + 'Sys',
+    'language': 'ENG',
+    'tZone': 'Europe/Rome',
+    'maxConnectTime': 30,  # [s]
+
+    'config_mysql_bk': {
+        'path': '',
+        'filename': '__mysql_' + job_id + 'bk.csv',
+        'topstring': '',
+        'max_rows_reload': 25,
+        'enable': options_['tempfileBk']
+    },
+    'config_data_bk': {
+        'path': '',
+        'filename': job_id + 'bk.csv',
+        'topstring': ';'.join(('Data', 'Device', 'idPar', 'Value', 'Code')),
+        'max_rows_reload': sample_params['sampleTime'] * 20
+    }
+}
+
+
 # List of devices
 idDevices_pack = {}
 
 idDevices_pack[0] = {
+
     'typeDev': 'P32',
     'idDev': 'd00',
     'method': 'tcp',
@@ -76,51 +160,3 @@ idDevices_pack[1] = {
     'timeout': 1,
     'enable': 1,
 }
-
-
-from decouple import config
-
-user_db = config('user_db', default='username')
-pswd_db = config('pswd_db', default='password')
-host_db = config('host_db', default='127.0.0.1')
-port_db = config('port_db', default='3306')
-job_id = config('job_id', default='000')
-
-res_data = {
-    'user': user_db,
-    'passwd': pswd_db,
-    'host': host_db,  # '10.8.0.1',
-    'port': port_db,
-}
-
-config_mysql = {
-    **res_data,
-    'database': 'seedb' + job_id,
-    'tableRaw': 'see' + job_id + 'Raw',
-    'tableDeps': 'see' + job_id + 'Deps',
-    'tableEvents': 'see' + job_id + 'Events',
-    'tableJoin': 'see' + job_id + 'Join',
-    'tableLast': 'see' + job_id + 'Last',
-    'tableMac': 'see' + job_id + 'Mac',
-    'tableParams': 'see' + job_id + 'Params',
-    'tableProd': 'see' + job_id + 'Prod',
-    'tableSysRaw': 'see' + job_id + 'SysSetRaw',
-    'language': 'ENG',
-    'tZone': 'Europe/Rome',
-
-    'config_mysql_bk': {
-        'path': '',
-        'filename': '__mysql_' + job_id + 'bk.csv',
-        'topstring': '',
-        'max_rows_reload': 25,
-        'enable': options_['tempfileBk']
-    },
-    'config_data_bk': {
-        'path': '',
-        'filename': job_id + 'bk.csv',
-        'topstring': ';'.join(('Data', 'Device', 'idPar', 'Value', 'Code')),
-        'max_rows_reload': sample_params['sampleTime'] * 20
-    }
-}
-
-
